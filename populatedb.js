e35fda846ad8bd356b792275037c3a4e6c5ad6af
Malcolm Foster <malcolmfoster@gmail.com>
2018-04-15 16:07:09 -0600

    :construction:

--- populatedb.js
+++ populatedb.js
@@ -1,16 +1,47 @@
-const express = require("express");
-const request = require('request');
-const app = express();
+const apiURL = "https://ftbserver.herokuapp.com/npos"
+const soda = require('soda-js');
 
-populateDB()
+var consumer = new soda.Consumer('data.colorado.gov');
 
-function populateDB(error, response, body) {
-    request('https://data.colorado.gov/resource/p3jp-z4tq.json?principalcity=Denver', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            // console.log(response)
-            var data = JSON.parse(response)
-            console.log(data)
-        }
+let stuff
+
+let npost = consumer.query()
+    .withDataset('p3jp-z4tq')
+    .limit(2)
+    .where({ principalcity: 'Denver' })
+    // .order('namelast')
+    .getRows()
+    .on('success', function (rows) {
+        rows.map((nposNew) => {
+            stuff = nposNew.name
+            // console.log(nposNew.fein);
+            
+            return stuff
     }) 
-}
+})
+    .on('error', function (error) { console.error(error); });
+// console.log(stuff);
 
+// console.log(npost);
+
+
+
+// let data = {
+//     fein: nposNew.fein,
+//     name: nposNew.name,
+//     revenuetotal: nposNew.revenuetotal,
+//     expensestotal: nposNew.expensestotal
+// }
+// fetch(apiURL, {
+//     method: 'POST',
+//     headers: new Headers({
+//         'Content-Type': 'application/json'
+//     }),
+//     body: JSON.stringify(data)
+// })
+//     .then(response => response.json())
+//     .then(response => {
+//         showSuccess(response.message)
+//         setTimeout(() => (removeMsg()), 4000);
+//     })
+//     .catch(console.error);
