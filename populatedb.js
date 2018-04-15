3000aeead565d9d8154e266bbddd5429dc63c1e2
Malcolm Foster <malcolmfoster@gmail.com>
2018-04-15 17:00:15 -0600

    :construction: working on POSTing

--- populatedb.js
+++ populatedb.js
@@ -11,35 +11,38 @@ consumer.query()
     .where({ principalcity: 'Denver' })
     // .order('namelast')
     .getRows()
-    .on('success': postData, function (rows) {
-        rows.map((nposNew) => {
-            
-    }) 
-})
+    .on('success', postData)
     .on('error', function (error) { console.error(error); });
 
-console.log(rows);
 
 
-let data = {
-    fein: nposNew.fein,
-    name: nposNew.name,
-    revenuetotal: nposNew.revenuetotal,
-    expensestotal: nposNew.expensestotal
-}
-fetch(apiURL, {
-    method: 'POST',
-    headers: new Headers({
-        'Content-Type': 'application/json'
-    }),
-    body: JSON.stringify(data)
-})
-    .then(response => response.json())
-    .then(response => {
-        showSuccess(response.message)
-        setTimeout(() => (removeMsg()), 4000);
+
+function postData(response) {
+    
+    console.log(response);
+    
+    let data = {
+        fein: response.fein,
+        name: response.name,
+        revenuetotal: response.revenuetotal,
+        expensestotal: response.expensestotal
+    }
+    fetch(apiURL, {
+        method: 'POST',
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify(data)
     })
-    .catch(console.error);
+        .then(response => response.json())
+        .then(response => {
+            showSuccess(response.message)
+            setTimeout(() => (removeMsg()), 4000);
+        })
+        .catch(console.error);
+}
+
+
 // console.log(stuff);
 
 // console.log(npost);
